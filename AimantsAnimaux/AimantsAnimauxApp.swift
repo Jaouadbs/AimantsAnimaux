acb9a05a420cedbe75bfd7f1e5be91bc55845df9
Jaouadbs <wk.boussalah@gmail.com>
2025-08-27 00:22:43 +0200

    Initial Commit

---
+++ AimantsAnimaux/AimantsAnimauxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AimantsAnimauxApp.swift
+//  AimantsAnimaux
+//
+//  Created by Jaouad on 27/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AimantsAnimauxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
